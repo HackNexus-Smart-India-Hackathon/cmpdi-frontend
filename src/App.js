3a96d314656994a1eced538a34ab9c7d38699f01
hustlernik <hustlernikxd@gmail.com>
2024-11-22 23:50:25 +0530

    chore: initial setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import AdminConsole from './pages/AdminConsole';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <p>cmpdi-frontend</p>
-    </div>
+    <Router>
+      <div>
+        <p className="flex justify-center items-center">cmpdi-frontend</p>
+        <button>
+          <Link to="/AdminConsole">click</Link>
+        </button>
+      </div>
+      <Routes>
+        <Route path="/AdminConsole" element={<AdminConsole />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/AdminConsole.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminConsole = () => {
+  return (
+    <div>
+      <h1>Admin Console</h1>
+    </div>
+  );
+};
+
+export default AdminConsole;

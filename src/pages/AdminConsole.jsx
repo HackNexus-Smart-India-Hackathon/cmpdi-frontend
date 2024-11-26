614ef14519a037fc5671781c30ee5d925482fc4c
hustlernik <hustlernikxd@gmail.com>
2024-11-26 23:28:35 +0530

    [FEAT] Sidebar UI

--- src/pages/AdminConsole.jsx
+++ src/pages/AdminConsole.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Sidebar from '../components/Sidebar';
 
 const AdminConsole = () => {
   return (
     <div>
       <h1>Admin Console</h1>
+      <Sidebar />
     </div>
   );
 };

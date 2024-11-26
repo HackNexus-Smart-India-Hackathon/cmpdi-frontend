ab19367418c1d579a7df2090411305426412a471
vipul <vipulkumar2426@gmail.com>
2024-11-26 18:39:06 +0530

    resolved conflict

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginSignup from './components/loginSignup/LoginSignup';
 import AdminConsole from './pages/AdminConsole';
 import './App.css';
-import LoginSignup from './components/loginSignup/LoginSignup';
 
 function App() {
   return (
-    // <Router>
-    //   <div>
-    //     <p className="flex justify-center items-center">cmpdi-frontend</p>
-    //     <button>
-    //       <Link to="/AdminConsole">Go to admin console</Link>
-    //     </button>
-    //   </div>
-    //   <Routes>
-    //     <Route path="/AdminConsole" element={<AdminConsole />} />
-    //   </Routes>
-    // </Router>
-    <div>
-      <LoginSignup />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginSignup />} />
+      </Routes>
+      <Routes>
+        <Route path="/AdminConsole" element={<AdminConsole />} />
+      </Routes>
+    </Router>
   );
 }
 

bd7b64e1f90e636481d1c9bcd6d28ca6852a88d0
Vipul <126176216+vipul6042@users.noreply.github.com>
2024-11-28 03:35:58 +0530

    Revert "[UPDATED]-SIGNUP"

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/loginSignup/Login';
-import ResetPassword from './components/loginSignup/ResetPassword';
-import SignUp from './components/loginSignup/SignUp';
-import TwoFactorAuth from './components/loginSignup/TwoFactorAuth';
+import LoginSignup from './components/loginSignup/LoginSignup';
 import AdminConsole from './pages/AdminConsole';
 import './App.css';
 
@@ -11,11 +8,10 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/resetpassword" element={<ResetPassword />} />
-        <Route path="/twofactorauthentication" element={<TwoFactorAuth />} />
-        <Route path="/adminconsole" element={<AdminConsole />} />
+        <Route path="/" element={<LoginSignup />} />
+      </Routes>
+      <Routes>
+        <Route path="/AdminConsole" element={<AdminConsole />} />
       </Routes>
     </Router>
   );

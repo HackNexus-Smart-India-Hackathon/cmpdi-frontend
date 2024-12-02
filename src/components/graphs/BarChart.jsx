3a85aa66f1896483ffd12bff612ec670d8f21c68
ItshMoh <mohan.kumar.min22@itbhu.ac.in>
2024-12-02 16:50:08 +0530

    made the components of piechart and Barchart

---
+++ src/components/graphs/BarChart.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Plot from "react-plotly.js";
+
+const BarChart = ({ data }) => {
+  if (!data || !Array.isArray(data) || data.length === 0) {
+    return <div>No data available</div>;
+  }
+
+  const xValues = data.map((item) => item.label);
+  const yValues = data.map((item) => item.value);
+
+  return (
+    <Plot
+      data={[
+        {
+          x: xValues,
+          y: yValues,
+          type: "bar",
+          marker: {
+            color: "blue",
+          },
+        },
+      ]}
+      layout={{
+        title: "Bar Chart",
+        xaxis: { title: "Categories" },
+        yaxis: { title: "Values" },
+        width: 600,
+        height: 400,
+      }}
+    />
+  );
+};
+
+export default BarChart;
